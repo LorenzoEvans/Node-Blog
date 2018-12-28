b21137077b737b95ac3fecbb8f78f13bcda8f0db
Lorenzo Evans <lorenzo.evans94@gmail.com>
2018-12-27 19:03:41 -0500

    Added middleware file, added script to package, currently setting up endpoints.

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
 const express = require("express")
 const helmet = require("helmet")
-const logger = require("morgan")
+const morganLog = require("morgan")
 const PORT = 4090
 
+const userDB = require("./data/helpers/userDb")
+const postDB = require("./data/helpers/postDb")
+
+server.use(
+ express.json(),
+ helmet(),
+ morganLog()
+)
+
+server.get('/api/users', (req, res, next) => {
+ userDB.get()
+ .then()
+ .catch()
+})
+
+server.get('/api/users/:id', (req, res, next) => {
+ const { id } = req.params
+ userDB.get(id)
+})
+
+server.post('/api/users', (req, res, next) => {
+ const { userId, text } = req.body
+ userDB.insert(post)
+ .then({userId, text})
+})
+
+server.put('/api/users/:id', (req, res, next) => {
+ userDB.update()
+})
 server.listen(PORT, () => {
  console.log(`Server running live on portn ${PORT}`)
 })
